3bb01586ba174eeb5d58daa12d1fb3b307543b1f
Samuel <samuel.muthaiga@student.moringaschool.com>
2023-05-28 23:37:09 +0300

    feat: generate our database schema for our models

--- db/schema.rb
+++ db/schema.rb
@@ -10,9 +10,42 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2023_05_27_223502) do
+ActiveRecord::Schema[7.0].define(version: 2023_05_28_192212) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
+
+  create_table "plumbers", force: :cascade do |t|
+    t.string "name"
+    t.string "email"
+    t.string "phone_number"
+    t.string "location"
+    t.string "specialization"
+    t.string "pricing"
+    t.float "start_rating"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["email"], name: "index_plumbers_on_email", unique: true
+  end
+
+  create_table "reviews", force: :cascade do |t|
+    t.bigint "user_id", null: false
+    t.bigint "plumber_id", null: false
+    t.integer "star_rating"
+    t.text "review_message"
+    t.boolean "endorsed"
+    t.boolean "recommended"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["plumber_id"], name: "index_reviews_on_plumber_id"
+    t.index ["user_id"], name: "index_reviews_on_user_id"
+  end
+
+  create_table "services", force: :cascade do |t|
+    t.string "name"
+    t.text "description"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
 
   create_table "users", force: :cascade do |t|
     t.string "name"
@@ -26,4 +59,6 @@ ActiveRecord::Schema[7.0].define(version: 2023_05_27_223502) do
     t.datetime "updated_at", null: false
   end
 
+  add_foreign_key "reviews", "plumbers"
+  add_foreign_key "reviews", "users"
 end
