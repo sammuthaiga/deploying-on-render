27db4c8094d01e6f8293ea24dc8c4a1f8641c4ba
Samuel <samuel.muthaiga@student.moringaschool.com>
2023-05-28 23:39:55 +0300

    feat: add routes to handle operations in our controllers

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,11 @@
 Rails.application.routes.draw do
   resources :users, param: :_username
+  resources :plumbers
+  resources :services, except: [:new, :edit]
+  resources :reviews, except: [:new, :edit]
+  resources :plumbers do
+    resources :availabilities, only: [:index, :show, :create, :update, :destroy]
+  end
   post '/auth/login', to: 'authentication#login'
   get '/*a', to: 'application#not_found'
 end
